be85f305785cf5ad4b26c9648b99550d7e997bc2
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2018-05-14 16:36:11 +0200

    bumped to version 0.1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dependencies = ['click', 'boto3', 'cfn-flip']
 
 setup(
     name='aws-cfn-update',
-    version="0.1.1",
+    version="0.1.2",
     url='https://github.com/mvanholsteijn/aws-cfn-update',
     license='BSD',
     author='Mark van Holsteijn',
