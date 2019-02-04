55d1644c85aef230ee5eb0d1a657321f90f3f651
Mark van Holsteijn <markvanholsteijn@binx.io>
2019-02-04 18:25:53 +0100

    support with/without fn-sub

--- aws_cfn_update/statemachine_updater.py
+++ aws_cfn_update/statemachine_updater.py
@@ -14,9 +14,13 @@
 #   Copyright 2018 binx.io B.V.
 import json
 import sys
+from collections import OrderedDict
 import click
+from ruamel.yaml.scalarstring import LiteralScalarString
 
 from aws_cfn_update.cfn_updater import CfnUpdater
+from cfn_updater import Sub
+
 
 class StateMachineDefinitionUpdater(CfnUpdater):
     """
@@ -29,44 +33,56 @@ class StateMachineDefinitionUpdater(CfnUpdater):
         self.definition = None
         self.definition_file = None
         self.template = None
+        self.with_fn_sub = True
         self.verbose = False
         self.dry_run = False
+
+
+    def new_value(self, definition):
+        if self.with_fn_sub:
+            return isinstance(definition, dict) and definition.get('Fn::Sub') == self.definition, {'Fn::Sub': self.definition}
+        else:
+            return definition == self.definition, self.definition
 
     def update_template(self):
         """
         updates the Definition of Step Function StateMachine.
         """
         resources = self.template.get('Resources', {})
-        for name, resource in filter(lambda k,v: k == self.resource_name, enumerate(resources)):
+        for name, resource in map(lambda n: (n, resources[n]), filter(lambda n: n == self.resource_name, resources)):
             if 'Properties' not in resource:
                 resource['Properties'] = {}
 
-            definition = resource['Properties'].get('DefinitionString')
-            if definition != self.definition:
+            same, new_definition = self.new_value(resource['Properties'].get('DefinitionString'))
+            if not same:
+                sys.stderr.write('INFO: updating definition of state machine {}\n'.format(self.resource_name))
+                resource['Properties']['DefinitionString'] = new_definition
+                self.dirty = True
+            else:
+                if self.verbose:
                     sys.stderr.write(
-                        'INFO: updating definition of state machine {} with {}\n'.format(self.resource_name,
-                                                                                         self.definition_file))
-                    resource['Properties']['DefinitionString'] = self.definition
-                    self.dirty = True
+                        'INFO: no changes to definition of state machine {}\n'.format(self.resource_name))
 
-    def read_definition(self):
+    def read_definition(self, filename):
+        self.definition_file = filename
         with open(self.definition_file, 'r') as f:
-            self.definition = json.load(f)
+            self.definition = LiteralScalarString(f.read())
 
-    def main(self, resource_name, definition_file, path, dry_run, verbose):
+    def main(self, resource_name, definition_file, with_fn_sub, path, dry_run, verbose):
         self.dry_run = dry_run
         self.verbose = verbose
         self.resource_name = resource_name
-        self.definition_file = definition_file
-        self.read_definition()
+        self.with_fn_sub = with_fn_sub
+        self.read_definition(definition_file)
         self.update(path)
 
 
 @click.command(name='state-machine-definition', help=StateMachineDefinitionUpdater.__doc__)
 @click.option('--resource', required=True, help='AWS::StepFunctions::StateMachine definition to update')
 @click.option('--definition', required=True, type=click.Path(exists=True), help='of the state machine')
+@click.option('--with-fn-sub/--without-fn-sub', required=False, default=True, help='for the definition')
 @click.argument('path', nargs=-1, required=True, type=click.Path(exists=True))
 @click.pass_context
-def update_state_machine_definition(ctx, resource, definition, path):
+def update_state_machine_definition(ctx, resource, definition, with_fn_sub, path):
     updater = StateMachineDefinitionUpdater()
-    updater.main(resource, definition, path, ctx.obj['dry_run'], ctx.obj['verbose'])
+    updater.main(resource, definition, with_fn_sub, list(path), ctx.obj['dry_run'], ctx.obj['verbose'])
