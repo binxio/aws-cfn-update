5631254f06ae9ecc5cbb4700874377147629bb1c
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2022-04-07 14:56:03 +0200

    fix: upgraded ruamel-yaml

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ Programmatically update CloudFormation templates
 """
 from setuptools import find_packages, setup
 
-dependencies = ['click', 'boto3', 'cfn-flip', 'pytz', 'pytest', 'croniter', 'tzlocal', 'click-datetime', 'ruamel.yaml==0.16.12', 'jsonmerge']
+dependencies = ['click', 'boto3', 'cfn-flip', 'pytz', 'pytest', 'croniter', 'tzlocal', 'click-datetime', 'ruamel.yaml', 'jsonmerge']
 
 setup(
     name='aws-cfn-update',
