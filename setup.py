b71dca345a0bc15b4d58c48f985ba748364267f5
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2022-09-04 17:18:23 +0200

    missing requests library

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ Programmatically update CloudFormation templates
 """
 from setuptools import find_packages, setup
 
-dependencies = ['click', 'boto3', 'cfn-flip', 'pytz', 'pytest', 'croniter', 'tzlocal', 'click-datetime', 'ruamel.yaml', 'jsonmerge', 'cryptography']
+dependencies = ['click', 'boto3', 'cfn-flip', 'pytz', 'pytest', 'croniter', 'tzlocal', 'click-datetime', 'ruamel.yaml', 'requests', 'jsonmerge', 'cryptography']
 
 setup(
     name='aws-cfn-update',
